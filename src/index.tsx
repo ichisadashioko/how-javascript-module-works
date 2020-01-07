25ab72456d166bf8addcb6958d66f928272c6348
Ichisada Shioko <ichisadashioko@gmail.com>
2020-01-07 17:41:34 +0700

    add redux dev tools

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, compose } from 'redux'
 
 import { rootReducer } from './store'
 import Slide from './components/slides/Slide'
 import { SlideNavigator } from './components/slides/Navigator'
 import { NEXT_SLIDE, PREVIOUS_SLIDE } from './store/presentation/types'
+import DevTools from './containers/DevTools'
 
-const store = createStore(rootReducer)
+const enhancer = compose(
+    // Required! Enable Redux DevTools with the monitors you chose
+    DevTools.instrument(),
+)
+
+const store = createStore(rootReducer, enhancer)
 
 render(
     <Provider store={store}>
@@ -17,6 +23,7 @@ render(
             onClickNext={() => store.dispatch({ type: NEXT_SLIDE })}
             onClickPrevious={() => store.dispatch({ type: PREVIOUS_SLIDE })}
         />
+        <DevTools />
     </Provider>,
     document.getElementById('root'),
 )
